b9d384eaaaa09bd572ebc0220a0b3b7198e91342
Julia Kuisma <julia.kuisma@virnex.fi>
2025-10-28 17:32:01 +0200

    2.1. step

---
+++ courceinfo/src/App.jsx
@@ -0,0 +1,68 @@
+const Course = (props) => {
+	return(
+		<>
+			<Header name={props.course.name} />
+			<Content parts={props.course.parts} />
+		</>
+	)
+}
+
+const Header = (props) => {
+	return(
+		<h1>{props.name}</h1>
+	)
+}
+
+const Content = (props) => {
+	return(
+		<>
+		{props.parts.map(part => (
+			<Part key={part.id} name={part.name} exercises={part.exercises} />
+		))}
+		</>
+	)
+}
+
+const Part = (props) => {
+	return(
+		<p>{props.name} {props.exercises}</p>
+	)
+}
+
+const Total = (props) => {
+	let total = 0;
+	props.course.parts.forEach(part => {
+		total += part.exercises;
+	});
+	return(
+		<p>Number of exercises: {total}</p>
+	)
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
